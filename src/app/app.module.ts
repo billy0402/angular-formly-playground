afc35dbe230883091cf0e670188c77dcdc3e32b8
Billy Huang <10646003@ntub.edu.tw>
2021-12-02 12:56:58 +0800

    chore(npm): add formly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    // Angular modules
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    // Third party modules
+    FormlyModule.forRoot({ extras: { lazyRender: true } }),
+    FormlyMaterialModule,
+
+    // Shared modules
+
+    // Components module
+
+    // App's modules
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
